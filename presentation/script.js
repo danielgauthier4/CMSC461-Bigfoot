c12c5f4a7f9f00d137886e8aa85ad246697fab8b
Campbell Jones <git@serebit.com>
2021-11-01 21:01:00 -0400

    Add some debugging stuff

--- presentation/script.js
+++ presentation/script.js
@@ -6,11 +6,18 @@ function setSingleCountyColor() {
     var map = document.getElementById("mapid");
 
     var doc = map.contentDocument;
+
+    console.log(map);
+
+    if (map.contentDocument === null || map.contentDocument === undefined) {
+        alert("Failed to apply style to SVG.");
+        return;
+    }
     var nevada = doc.getElementById("057 Nevada");
     var alameda = doc.getElementById("001 Alameda");
     var alpine = doc.getElementById("003 Alpine");
 
     nevada.style["fill"] = "green";
     alameda.style["fill"] = "blue";
-    alpine.style["fill"] = "red";
+    alpine.style["fill"] = "red"; 
 }
