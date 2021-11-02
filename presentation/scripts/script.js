0a7636cf4ae3aa3659e8815c38fafad21a74c97d
Campbell Jones <git@serebit.com>
2021-11-02 14:23:26 -0400

    Add tooltip on county hover

--- presentation/scripts/script.js
+++ presentation/scripts/script.js
@@ -18,10 +18,21 @@ function setSingleCountyColor() {
 
     var counties = Array.from(doc.getElementsByClassName("county"));
 
+    var tip = document.createElement("div");
+    tip.classList.add("tooltip");
+    document.body.appendChild(tip);
+
     counties.forEach(element => {
         element.style["fill"] = 'hsl(192,80%,'+(Math.random()*70+15)+'%)';
+
+        element.addEventListener("mousemove", (evt) => {
+            tip.style.left = evt.screenX + 'px';
+            tip.style.top = (evt.screenY + evt.clientY) / 2 + 'px';
+            tip.innertText = evt.screenX;
+            console.log("moved");
+        }, false);
         
-        element.addEventListener("mouseover", (evt) => {
+        element.addEventListener("mouseenter", (evt) => {
             element.style["stroke"] = '#fff';
             element.style["stroke-width"] = '.5';
 
@@ -29,11 +40,17 @@ function setSingleCountyColor() {
 
             parent.removeChild(element);
             parent.appendChild(element);
-        });
+
+            console.log(evt);
+
+            tip.innerText = element.id;
+            tip.style.visibility = "unset";
+        }, false);
 
         element.addEventListener("mouseout", (evt) => {
             element.style["stroke"] = '#000';
             element.style["stroke-width"] = '.1';
-        });
+            tip.style.visibility = "hidden";
+        }, false);
     });
 }
