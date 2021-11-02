1147fc323fc6389ee79bad7561220262940a847d
Campbell Jones <git@serebit.com>
2021-11-02 11:20:21 -0400

    Show a white border on hover over county

--- presentation/scripts/script.js
+++ presentation/scripts/script.js
@@ -20,5 +20,20 @@ function setSingleCountyColor() {
 
     counties.forEach(element => {
         element.style["fill"] = 'hsl(192,80%,'+(Math.random()*70+15)+'%)';
+        
+        element.addEventListener("mouseover", (evt) => {
+            element.style["stroke"] = '#fff';
+            element.style["stroke-width"] = '.5';
+
+            var parent = element.parentNode;
+
+            parent.removeChild(element);
+            parent.appendChild(element);
+        });
+
+        element.addEventListener("mouseout", (evt) => {
+            element.style["stroke"] = '#000';
+            element.style["stroke-width"] = '.1';
+        });
     });
 }
