82525846d9ee257f5f5208b2f6c07b481f5d94d9
Campbell Jones <git@serebit.com>
2021-11-02 00:20:42 -0400

    Modify script to apply colors to all counties on date select

--- presentation/script.js
+++ presentation/script.js
@@ -1,23 +1,24 @@
-window.addEventListener("load", setSingleCountyColor, false);
+window.addEventListener("load", prepWindow, false);
+
+function prepWindow() {
+    setSingleCountyColor();
+
+    document.getElementById("date").addEventListener("input", setSingleCountyColor, false);
+}
 
 function setSingleCountyColor() {
-    console.log("running");
-
     var map = document.getElementById("mapid");
 
     var doc = map.contentDocument;
-
-    console.log(map);
 
     if (map.contentDocument === null || map.contentDocument === undefined) {
         alert("Failed to apply style to SVG.");
         return;
     }
-    var nevada = doc.getElementById("057 Nevada");
-    var alameda = doc.getElementById("001 Alameda");
-    var alpine = doc.getElementById("003 Alpine");
 
-    nevada.style["fill"] = "green";
-    alameda.style["fill"] = "blue";
-    alpine.style["fill"] = "red"; 
+    var counties = Array.from(doc.getElementsByClassName("county"));
+
+    counties.forEach(element => {
+        element.style["fill"] = 'hsl(192,80%,'+(Math.random()*70+15)+'%)';
+    });
 }
