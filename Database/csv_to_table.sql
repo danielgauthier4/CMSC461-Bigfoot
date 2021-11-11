e33a0e9bb136d86dfabc97af6f25bf6c56ad207e
ZackKirychuk <copperhail24@gmail.com>
2021-11-11 18:46:21 -0500

    -Added id to tables -Changed columns to lowercase -Added NULL checking

--- Database/csv_to_table.sql
+++ Database/csv_to_table.sql
@@ -3,6 +3,8 @@ TO RUN THIS SCRIPT
 	-Download MySQL.
 	-Enter cmd prompt and add MySQL path to PATH 
 		(e.g. for Windows: "set PATH=%PATH%;C:\Program Files\MySQL\MySQL Server 8.0\bin")
+	-Enable local_infile globally in mysql and then close mysql
+		(SET GLOBAL local_infile=1)
 	-cd to this script's directory and run MySQL client with --local_infile flag enabled
 		("mysql --local_infile=1 -u root -p")
 	-Run the script
@@ -15,48 +17,56 @@ CREATE DATABASE covid_data;
 USE covid_data;
 
 CREATE TABLE `covid_data`.`nyt_counts` (
-  `date` VARCHAR(45) NULL,
-  `county` VARCHAR(45) NULL,
-  `state` VARCHAR(45) NULL,
-  `fips` VARCHAR(45) NULL,
-  `cases` INT NULL,
-  `deaths` INT NULL);
+  id  int(1) unsigned not null AUTO_INCREMENT,
+  `date` DATETIME NULL,
+  `county` VARCHAR(45) NOT NULL DEFAULT '0',
+  `state` VARCHAR(45) NOT NULL DEFAULT '0',
+  `fips` VARCHAR(45) NOT NULL DEFAULT '0',
+  `cases` INT NOT NULL DEFAULT 0,
+  `deaths` INT NOT NULL DEFAULT 0,
+  primary key (id));
   
 
 CREATE TABLE `covid_data`.`national_counts` (
-  `Date` DATETIME NULL,
-  `Measure` VARCHAR(45) NULL,
-  `Count` INT NULL,
-  `Reporting` INT NULL,
-  `Missing` VARCHAR(1024) NULL);
+  id  int(2) unsigned not null AUTO_INCREMENT,
+  `date` DATETIME NULL,
+  `measure` VARCHAR(45) NOT NULL DEFAULT '0',
+  `count` INT NOT NULL DEFAULT 0,
+  `reporting` INT NOT NULL DEFAULT 0,
+  `missing` VARCHAR(1024) NOT NULL DEFAULT '0',
+  primary key (id));
 
 
 CREATE TABLE `covid_data`.`state_jurisdiction_counts` (
-  `State` VARCHAR(45) NULL,
-  `Web.Group` VARCHAR(45) NULL,
-  `MEASURE` VARCHAR(45) NULL,
-  `VAL` INT NULL,
-  `Rate` VARCHAR(45) NULL,
-  `Date` DATETIME NULL);
+  id  int(3) unsigned not null AUTO_INCREMENT,
+  `state` VARCHAR(45) NOT NULL DEFAULT '0',
+  `web.group` VARCHAR(45) NOT NULL DEFAULT '0',
+  `measure` VARCHAR(45) NOT NULL DEFAULT '0',
+  `val` INT NOT NULL DEFAULT 0,
+  `rate` VARCHAR(45) NOT NULL DEFAULT '0',
+  `date` DATETIME NULL,
+  primary key (id));
   
 
 CREATE TABLE `covid_data`.`state_counts` (
-  `Date` DATETIME NULL,
-  `State` VARCHAR(45) NULL,
-  `Residents.Confirmed` VARCHAR(45) NULL,
-  `Staff.Confirmed` VARCHAR(45) NULL,
-  `Residents.Deaths` VARCHAR(45) NULL,
-  `Staff.Deaths` VARCHAR(45) NULL,
-  `Residents.Tadmin` VARCHAR(45) NULL,
-  `Residents.Tested` VARCHAR(45) NULL,
-  `Residents.Active` VARCHAR(45) NULL,
-  `Staff.Active` VARCHAR(45) NULL,
-  `Staff.Initiated` VARCHAR(45) NULL,
-  `Residents.Initiated` VARCHAR(45) NULL,
-  `Residents.Completed` VARCHAR(45) NULL,
-  `Staff.Completed` VARCHAR(45) NULL,
-  `Residents.Vadmin` VARCHAR(45) NULL,
-  `Staff.Vadmin` VARCHAR(45) NULL);
+  id  int(4) unsigned not null AUTO_INCREMENT,
+  `date` DATETIME NULL,
+  `state` VARCHAR(45) NOT NULL DEFAULT '0',
+  `residents.confirmed` VARCHAR(45) NOT NULL DEFAULT '0',
+  `staff.confirmed` VARCHAR(45) NOT NULL DEFAULT '0',
+  `residents.deaths` VARCHAR(45) NOT NULL DEFAULT '0',
+  `staff.deaths` VARCHAR(45) NOT NULL DEFAULT '0',
+  `residents.tadmin` VARCHAR(45) NOT NULL DEFAULT '0',
+  `residents.tested` VARCHAR(45) NOT NULL DEFAULT '0',
+  `residents.active` VARCHAR(45) NOT NULL DEFAULT '0',
+  `staff.active` VARCHAR(45) NOT NULL DEFAULT '0',
+  `staff.initiated` VARCHAR(45) NOT NULL DEFAULT '0',
+  `residents.initiated` VARCHAR(45) NOT NULL DEFAULT '0',
+  `residents.completed` VARCHAR(45) NOT NULL DEFAULT '0',
+  `staff.completed` VARCHAR(45) NOT NULL DEFAULT '0',
+  `residents.vadmin` VARCHAR(45) NOT NULL DEFAULT '0',
+  `staff.vadmin` VARCHAR(45) NOT NULL DEFAULT '0',
+  primary key (id));
   
 
 LOAD DATA LOCAL INFILE  
@@ -76,7 +86,7 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(Date, Measure, Count, Reporting, Missing);
+(date, measure, count, reporting, missing);
 
 
 LOAD DATA LOCAL INFILE  
@@ -88,20 +98,20 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
 (Date,
 State,
-`Residents.Confirmed`,
-`Staff.Confirmed`,
-`Residents.Deaths`,
-`Staff.Deaths`,
-`Residents.Tadmin`,
-`Residents.Tested`,
-`Residents.Active`,
-`Staff.Active`,
-`Staff.Initiated`,
-`Residents.Initiated`,
-`Residents.Completed`,
-`Staff.Completed`,
-`Residents.Vadmin`,
-`Staff.Vadmin`);
+`residents.confirmed`,
+`staff.confirmed`,
+`residents.deaths`,
+`staff.deaths`,
+`residents.tadmin`,
+`residents.tested`,
+`residents.active`,
+`staff.active`,
+`staff.initiated`,
+`residents.initiated`,
+`residents.completed`,
+`staff.completed`,
+`residents.vadmin`,
+`staff.vadmin`);
 
 
 LOAD DATA LOCAL INFILE  
