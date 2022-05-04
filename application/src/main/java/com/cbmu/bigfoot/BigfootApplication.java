30bdc0ec49c74b03e324a1f89a4907e3794101fd
Dan Gauthier <dancgauthier@gmail.com>
2022-05-03 22:00:41 -0400

    Initial Version

---
+++ application/src/main/java/com/cbmu/bigfoot/BigfootApplication.java
@@ -0,0 +1,13 @@
+package com.cbmu.bigfoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigfootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigfootApplication.class, args);
+	}
+
+}
